326e3eee6eb49bc06d211e00052448fae45ea2cd
mercy <89814009+mercyprevails@users.noreply.github.com>
2021-09-01 13:01:31 -0400

    revision 1 - added first loop of main song, finishing full baseline to sync with a few-second mini tune later. - added notation - rm documentation

--- Experiments/megalovania.ino
+++ Experiments/megalovania.ino
@@ -1,63 +1,88 @@
 /*
-  Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
+  Arduino UNO Megalovania
+  Never coded before, fix my mistakes if found.
 */
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
+  pinMode(1, OUTPUT); // (A) [lower octave]
+  pinMode(2, OUTPUT); // (B)
+  pinMode(3, OUTPUT); // (C)
+  pinMode(4, OUTPUT); // (D) [[starting note]]
+  pinMode(5, OUTPUT); // (F)
+  pinMode(6, OUTPUT); // (G)(G#) G# is a slide from A to G done in one note timespan
+  pinMode(7, OUTPUT); // (A) 
+  pinMode(8, OUTPUT); // (D) [higher octave]
 }
 
-// the loop function runs over and over again forever
+// the loop function runs over and over again forever 
+
+/* Song code where delay(#.#) ≈ note length from mp3
+   in ms + 15% to factor for your eye seeing the LED.
+*/
+
 void loop() { 
-  turnOn();
-  delay(105);
-  turnOff();
-  delay(69);
-    turnOn();
-  delay(105);
-  turnOff();
-  delay(75);
-  turnOn();
-  delay(105);
-  turnOff();
-  delay(280);
-  turnOn();
-  delay(105);
-  turnOff();
-  delay(1000);
+
+  digitalWrite(4, HIGH); // (D)
+  delay(87.5);
+  digitalWrite(4, LOW);
+  delay(45);
+
+  digitalWrite(4, HIGH); // (D)
+  delay(87.5);
+  digitalWrite(4, LOW);
+  delay(45);
+  
+  digitalWrite(8, HIGH); // (D^2)
+  delay(87.5);
+  digitalWrite(8, LOW);
+  delay(90);
+  
+  digitalWrite(7, HIGH); // (A)
+  delay(87.5);
+  digitalWrite(7, LOW);
+  delay(190);
+  
+  digitalWrite(5, HIGH); // (G#) SLIDE BLOCK
+  delay(43.75);
+  digitalWrite(5, LOW);
+  digitalWrite(6, HIGH);
+  delay(43.75);
+  digitalWrite(6, LOW);
+  delay(90);
+  
+  digitalWrite(6, HIGH); // (G)
+  delay(87.5);
+  digitalWrite(6, LOW);
+  delay(90);
+  
+  digitalWrite(5, HIGH); // (F)
+  delay(247.25);
+  digitalWrite(5, LOW);
+  delay(45);
+  
+  digitalWrite(4, HIGH); // (D)
+  delay(87.5);
+  digitalWrite(4, LOW);
+  delay(45);
+  
+  digitalWrite(5, HIGH); // (F)
+  delay(87.5);
+  digitalWrite(5, LOW);
+  delay(45);
+  
+  digitalWrite(6, HIGH); // (G)
+  delay(87.5);
+  digitalWrite(6, LOW);
+  delay(45);
+  
 }
 
 void turnOn(){
   digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(1, HIGH);
   digitalWrite(2, HIGH);
   digitalWrite(3, HIGH);
   digitalWrite(4, HIGH);
@@ -65,11 +90,11 @@ void turnOn(){
   digitalWrite(6, HIGH);
   digitalWrite(7, HIGH);
   digitalWrite(8, HIGH);
-  digitalWrite(9, HIGH);
 }
 
 void turnOff(){
   digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(1, LOW);
   digitalWrite(2, LOW);
   digitalWrite(3, LOW);
   digitalWrite(4, LOW);
@@ -77,5 +102,4 @@ void turnOff(){
   digitalWrite(6, LOW);
   digitalWrite(7, LOW);
   digitalWrite(8, LOW);
-  digitalWrite(9, LOW);
 }
