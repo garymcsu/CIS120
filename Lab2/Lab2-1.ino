60ba736663388f4fe1fc729484f4cdddf67b5d4b
mercy <89814009+mercyprevails@users.noreply.github.com>
2021-09-08 18:30:54 -0400

    Lab 2 Final Revision - Buzzer sounds alarm! - Red light flashes

--- Lab2/Lab2-1.ino
+++ Lab2/Lab2-1.ino
@@ -1,14 +1,31 @@
+/* Lab 2 Final Code
+   Blinking Buzzer
+ */
+
 int lightLevel;
 
 void setup() {
   pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
 }
 
 void loop() {
   lightLevel = analogRead(A0);
-  if (lightLevel > 300) {
-    digitalWrite(9, LOW);
+  if (lightLevel > 900) {
+    noTone(10);
+  digitalWrite(9, LOW);
   } else {
+  tone(10, 100);
     digitalWrite(9, HIGH);
+  delay(100);
+    
+  noTone(10);
+    digitalWrite(9, LOW);
+  delay(100);
   }
+  
 }
+
+ 
+
+ 
