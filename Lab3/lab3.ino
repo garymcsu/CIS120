c090e5b1cc3880e9c97ae988c4e3aa389d401f3f
Gary Montgomery <89813707+garymcsu@users.noreply.github.com>
2021-09-20 17:47:46 -0400

    Update Lab3 with correct code
    includes a while loop that fires only when the button is pressed

--- Lab3/lab3.ino
+++ Lab3/lab3.ino
@@ -1,82 +1,45 @@
-/*
-  SparkFun Inventor’s Kit
-  Circuit 4B - Temperature Sensor
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
 
-  The LCD will display readings from a temperature sensor in degrees Celsius and Fahrenheit.
-
-  This sketch was written by SparkFun Electronics, with lots of help from the Arduino community.
-  This code is completely free for any use.
-
-  View circuit diagram and instructions at: https://learn.sparkfun.com/tutorials/sparkfun-inventors-kit-experiment-guide---v41
-  Download drawings and code at: https://github.com/sparkfun/SIK-Guide-Code
-*/
-
-#include <LiquidCrystal.h>                  //the liquid crystal library contains commands for printing to the display
-LiquidCrystal lcd(13, 12, 11, 10, 9, 8);    // tell the RedBoard what pins are connected to the display
-
-const int buttonPin = 4;
-int buttonState;
-int lastButtonState = LOW;
-
-// the following variables are unsigned longs because the time, measured in
-// milliseconds, will quickly become a bigger number than can be stored in an int.
-unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
-unsigned long debounceDelay = 50; // the debounce time; increase if the output flickers
+const int buttonPin = 2;
 
 float voltage = 0;                          //the voltage measured from the TMP36
 float degreesC = 0;                         //the temperature in Celsius, calculated from the voltage
-float degreesF = 0;                         //the temperature in Fahrenheit, calculated from the voltage
+float degreesF = 0; 
 
 void setup() {
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT_PULLUP);
 
-  //First show on screen to please press the button
-  //Maybe add a loading bar when the button is being pressed
-  pinMode(buttonPin, INPUT);
-  
-  lcd.begin(16, 2);                         //tell the lcd library that we are using a display that is 16 characters wide and 2 characters high
-  lcd.clear();                              //clear the display
+  lcd.begin(16, 2);
 }
 
 void loop() {
-  // read the state of the switch into a local variable;
-  int reading = digitalRead(buttonPin);
-
-  if(reading != lastButtonState){
-    // reset the debouncing timer
-    lastDebounceTime = millis();
+  lcd.clear();
+  while (digitalRead(buttonPin) == LOW) {
+    displayTemp();
   }
+ displayPressButton();
+}
 
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    // whatever the reading is at, it's been there for longer than the debounce
-    // delay, so take it as the actual current state:
+void displayPressButton(){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Press Button");
+  delay(1000);
+}
 
-    // if the button state has changed:
-    if (reading != buttonState) {
-      buttonState = reading;
+void displayTemp(){
+    voltage = analogRead(A0) * 0.004882813;   //convert the analog reading, which varies from 0 to 1023, back to a voltage value from 0-5 volts
+    degreesC = (voltage - 0.5) * 100.0;       //convert the voltage to a temperature in degrees Celsius
+    degreesF = degreesC * (9.0 / 5.0) + 32.0; //convert the voltage to a temperature in degrees Fahrenheit
 
-      // only toggle the LED if the new button state is HIGH
-      if (buttonState == HIGH) {
-        ledState = !ledState;
-      }
-    }
-  }
-
-  voltage = analogRead(A0) * 0.004882813;   //convert the analog reading, which varies from 0 to 1023, back to a voltage value from 0-5 volts
-  degreesC = (voltage - 0.5) * 100.0;       //convert the voltage to a temperature in degrees Celsius
-  degreesF = degreesC * (9.0 / 5.0) + 32.0; //convert the voltage to a temperature in degrees Fahrenheit
-
-  lcd.clear();                              //clear the LCD
-
-  lcd.setCursor(0, 0);                      //set the cursor to the top left position
-  lcd.print("Degrees C: ");                 //print a label for the data
-  lcd.print(degreesC);                      //print the degrees Celsius
-
-  lcd.setCursor(0, 1);                      //set the cursor to the lower left position
-  lcd.print("Degrees F: ");                 //Print a label for the data
-  lcd.print(degreesF);                      //print the degrees Fahrenheit
-
-  delay(1000);                              //delay for 1 second between each reading (this makes the display less noisy)
-  
-  // save the reading. Next time through the loop, it'll be the lastButtonState:
-  lastButtonState = reading;
+    lcd.clear();                              //clear the LCD
+    lcd.setCursor(0, 0);                      //set the cursor to the top left position
+    lcd.print("Degrees C: ");                 //print a label for the data
+    lcd.print(degreesC);                      //print the degrees Celsius
+    lcd.setCursor(0, 1);                      //set the cursor to the lower left position
+    lcd.print("Degrees F: ");                 //Print a label for the data
+    lcd.print(degreesF);  
+    delay(1000);
 }
