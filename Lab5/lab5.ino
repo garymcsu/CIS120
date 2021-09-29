3ff6a567790a17ea1f5b0a670175980b60b6cee6
Gary Montgomery <89813707+garymcsu@users.noreply.github.com>
2021-09-29 10:57:34 -0400

    Update Lab5
    add beep method

--- Lab5/lab5.ino
+++ Lab5/lab5.ino
@@ -1,2 +1,24 @@
-void setup(){}
-void loop(){}
+// Buzzer Pin = 40
+// Joystick vars:
+// #define joystickX 2
+// #define joystickY 26
+// uint16_t x, y, x00, y00;
+// uint16_t colour;
+// uint32_t z;
+
+int buzzerPin = 40;
+
+void setup(){
+  pinMode(buzzerPin,OUTPUT);  
+}
+void loop(){
+  beep(440, 500);
+  delay(2000);  
+}
+
+void beep(int note, int duration){
+  tone(buzzerPin, note, duration/2);
+  delay(duration/2);
+  noTone(buzzerPin);
+  delay(duration/2 + 20);  
+}
