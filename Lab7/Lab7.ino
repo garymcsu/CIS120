e734be50e9c267aa937e996e7d7ef7b8409cf09d
Gary Montgomery <89813707+garymcsu@users.noreply.github.com>
2021-10-13 18:50:14 -0400

    Update Lab7.ino

--- Lab7/Lab7.ino
+++ Lab7/Lab7.ino
@@ -1,9 +1,61 @@
-void setup() {
-  // put your setup code here, to run once:
+const int buttonOne = 33;     // the number of the pushbutton pin
+const int buttonTwo = 32;
 
+const int ledGreen =  38;      // the number of the LED pin
+const int ledBlue =  37;
+
+int buttonOneState = 0;         // variable for reading the pushbutton #1 status
+int buttonTwoState = 0;
+
+int currentNumber = 0;
+
+void setup() {
+  pinMode(ledGreen, OUTPUT);
+  pinMode(ledBlue, OUTPUT);
+  // put your setup code here, to run once:
+  pinMode(buttonOne, INPUT_PULLUP);
+  pinMode(buttonTwo, INPUT_PULLUP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly: 
-  
+  // put your main code here, to run repeatedly:
+  buttonOneState = digitalRead(buttonOne);
+
+  if (buttonOneState == LOW) {
+    increment();
+    digitalWrite(ledGreen, HIGH);
+  }
+  else {
+    digitalWrite(ledGreen, LOW);
+  }
+
+  buttonTwoState = digitalRead(buttonTwo);
+
+  if (buttonTwoState == LOW) {
+    decrement();
+    digitalWrite(ledBlue, HIGH);
+  }
+  else {
+    digitalWrite(ledBlue, LOW);
+  }
+}
+
+void increment() {
+  if (currentNumber < 100) {
+    if (currentNumber == 99) {
+      currentNumber = 0;
+    } else {
+      currentNumber++;
+    }
+  }
+}
+
+void decrement() {
+  if (currentNumber >= 0) {
+    if (currentNumber == 0) {
+      currentNumber = 99;
+    } else {
+      currentNumber--
+    }
+  }
 }
