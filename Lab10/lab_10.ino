05999239d04fff6519b0bd1c97651ee6e7ee04fd
ActionJackson35 <89813858+ActionJackson35@users.noreply.github.com>
2021-11-03 19:17:27 -0400

    Final revision -delay

--- Lab10/lab_10.ino
+++ Lab10/lab_10.ino
@@ -6,14 +6,14 @@ LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
 
 UltraDistSensor mysensor;
 float reading;
+float rreading;
 int LED = 2;
 int Buzzer = 7;
-
 const int buttonPin = 2;
 
 void setup() {
-  pinMode(LED, OUTPUT)
-  pinMode(Buzzer, OUTPUT)
+  pinMode(LED, OUTPUT);
+  pinMode(Buzzer, OUTPUT);
   Serial.begin(9600);
   mysensor.attach(4,5);//Trigger pin , Echo pin
   lcd.begin(16, 2);
@@ -21,27 +21,26 @@ void setup() {
 
 void loop() {
     reading=mysensor.distanceInCm();
+    rreading = reading * 0.393701; //Conversion from cm to in
      lcd.setCursor(0,0);
-     lcd.print("Distance :- ");
-      lcd.print(reading);
-
-    delay(1000);
-    if(distance < 6.5){
+     lcd.print("Distance : ");
+     lcd.setCursor(0, 1);  
+     lcd.print(rreading);
+     lcd.print("   Inches");
+     
+    delay(100);
+    
+    if(rreading < 6.5){
       digitalWrite(LED, HIGH);
-      }
+      tone(Buzzer, 500);
+      delay(25);
+      digitalWrite(LED, LOW);
+      noTone(Buzzer);
+      delay(25);
+    }
     else {
+      noTone(Buzzer);
       digitalWrite(LED, LOW);
     }
-    
-    if (distance > 6.5) {
-      noTone(Buzzer);
-    }
-    else {
-      tone(Buzzer, 100);
-      delay(100);
-      noTone(Buzzer);
-      delay(100);
-    }
-  
-  }
+
 }
