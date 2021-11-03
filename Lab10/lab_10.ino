cf8e878e0b17d2ea6ad6fabf261554ce55b60a22
mercy <89814009+mercyprevails@users.noreply.github.com>
2021-11-03 18:43:19 -0400

    LED

--- Lab10/lab_10.ino
+++ Lab10/lab_10.ino
@@ -1,16 +1,17 @@
 #include <LiquidCrystal.h>
 #include <UltraDistSensor.h>
 #include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
 
 LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
 
 UltraDistSensor mysensor;
 float reading;
+int LED = 2;
 
 const int buttonPin = 2;
 
 void setup() {
+  pinMode(LED, OUTPUT)
   Serial.begin(9600);
   mysensor.attach(4,5);//Trigger pin , Echo pin
   lcd.begin(16, 2);
@@ -23,4 +24,7 @@ void loop() {
       lcd.print(reading);
 
     delay(1000);
+    if(distance < 7){
+      digitalWrite(LED, HIGH);
+      }
 }
