29b7225bbaa299857efd205a989fce160aa11bcf
mercy <89814009+mercyprevails@users.noreply.github.com>
2021-11-03 18:51:23 -0400

    Buzzer

--- Lab10/lab_10.ino
+++ Lab10/lab_10.ino
@@ -7,11 +7,13 @@ LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
 UltraDistSensor mysensor;
 float reading;
 int LED = 2;
+int Buzzer = 7;
 
 const int buttonPin = 2;
 
 void setup() {
   pinMode(LED, OUTPUT)
+  pinMode(Buzzer, OUTPUT)
   Serial.begin(9600);
   mysensor.attach(4,5);//Trigger pin , Echo pin
   lcd.begin(16, 2);
@@ -24,10 +26,22 @@ void loop() {
       lcd.print(reading);
 
     delay(1000);
-    if(distance < 7){
+    if(distance < 6.5){
       digitalWrite(LED, HIGH);
       }
     else {
       digitalWrite(LED, LOW);
     }
+    
+    if (distance > 6.5) {
+    noTone(Buzzer);
+    }
+    else {
+  tone(Buzzer, 100);
+  delay(100);
+  noTone(Buzzer);
+  delay(100);
+  }
+  
+}
 }
