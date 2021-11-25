64515ffe35aeb72de527bd78ba6a40b133a07a98
Gary Montgomery <89813707+garymcsu@users.noreply.github.com>
2021-11-24 19:35:16 -0500

    refactor and correct formatting errors

--- Final Project/final.ino
+++ Final Project/final.ino
@@ -1,8 +1,8 @@
 #include <Servo.h>
 
-const int trigPin=8;
-const int echoPin=7;
-const int buzzpin=3;
+const int trigPin = 8;
+const int echoPin = 7;
+const int buzzpin = 3;
 
 long duration;
 int distance;
@@ -10,45 +10,46 @@ int distance;
 Servo s1;
 
 void setup() {
-  
+
   Serial.begin(9600);
   pinMode(buzzpin, OUTPUT);
-  pinMode(trigPin,OUTPUT);
-  pinMode(echoPin,INPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 
   s1.attach(9);
 }
 
 void loop()
 {
-    for(int i=0;i<=180;i++){            // rotates the servo motor from 15 to 165 degrees
-      s1.write(i);
-      delay(30);
-      distance = calDist();
-      Serial.print(i);                      // Sends the current degree into the Serial Port
-      Serial.print(",");                   // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
-      Serial.print(distance);                // Sends the distance value into the Serial Port
-      Serial.print(".");                   // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
+  for (int i = 0; i <= 180; i++) {    // rotates the servo motor from 15 to 165 degrees
+    s1.write(i);
+    delay(30);
+    distance = calDist();
+    Serial.print(i);                      // Sends the current degree into the Serial Port
+    Serial.print(",");                   // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
+    Serial.print(distance);                // Sends the distance value into the Serial Port
+    Serial.print(".");                   // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
     if (distance <= 16.51) {
       tone(buzzpin, 932, 45);
-      millis(500);
-    else{
+      millis();
+    } else {
       noTone(buzzpin);
     }
   }
 
-    for(int i=180;i>0;i--){  
-      s1.write(i);
-      delay(30);
-      distance = calDist();
-      Serial.print(i);
-      Serial.print(",");
-      Serial.print(distance);
-      Serial.print(".");
+
+  for (int i = 180; i > 0; i--) {
+    s1.write(i);
+    delay(30);
+    distance = calDist();
+    Serial.print(i);
+    Serial.print(",");
+    Serial.print(distance);
+    Serial.print(".");
     if (distance <= 16.51) {
       tone(buzzpin, 932, 45);
-      millis(500);
-    else{
+      millis();
+    } else {
       noTone(buzzpin);
     }
   }
@@ -57,16 +58,16 @@ void loop()
 
 int calDist()
 {
-   digitalWrite(trigPin, LOW);
-   delayMicroseconds(2);
-                                                                // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW); 
-                                                              // Reads the echoPin, returns the sound wave travel time in microseconds
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(echoPin, HIGH);
-                                                              // Calculating the distance
-  distance= duration*0.034/2;                                // Range : 2cm to 400 cm
+  // Calculating the distance
+  distance = duration * 0.034 / 2;                           // Range : 2cm to 400 cm
 
   return distance;
 }
